7f27fc6cb8b1d9f68699b147bbc1a04d69e9ab54
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-07-24 15:57:30 +0300

    Доработка логики обработки команд

--- FileManagerOOP/FileManagerLogic.cs
+++ FileManagerOOP/FileManagerLogic.cs
@@ -1,4 +1,6 @@
-﻿namespace FileManagerOOP;
+﻿using FileManagerOOP.Base;
+using FileManagerOOP.Base.Comands;
+namespace FileManagerOOP;
 
 public class FileManagerLogic
 {
@@ -6,9 +8,15 @@ public class FileManagerLogic
 
     public DirectoryInfo CurrentDirectory { get; set; } = new("c:\\");
 
+    public IReadOnlyDictionary<string, FileManagerComand> Commands { get; }
     public FileManagerLogic(IUserInterface userInterface)
     {
         _userInterface = userInterface;
+        Commands = new Dictionary<string, FileManagerComand>()
+        {
+            { "drives", new DrivesListComand(userInterface) },
+            { "dir", new PrintDirectoryFilesCommand(userInterface,this) }
+        };
     }
     public void Start()
     {
@@ -17,21 +25,31 @@ public class FileManagerLogic
         bool can_work = true;
         do
         {
-            var input = _userInterface.ReadLine("> ", false);
+            var input = _userInterface.ReadLine("> ", false); 
 
-            switch (input)
+            var args = input.Split(' ');
+            var commandName = args[0];
+
+            if (commandName == "quit") //РЕАЛИЗОВАТЬ КОМАНДОЙ
             {
-                case "q":
-                    can_work = false;
-                    break;
-                case "int":
-                    int int_value  = _userInterface.ReadInt("Введите целое число");
-                    _userInterface.WriteLine($"Введено: {int_value}");  
-                    break;
-                case "double":
-                    double double_value = _userInterface.ReadDouble("Введите целое число");
-                    _userInterface.WriteLine($"Введено: {double_value}");
-                    break;
+                can_work = false;
+                continue;
+            }
+
+            if(!Commands.TryGetValue(commandName, out var command))
+            {
+                _userInterface.WriteLine($"Неизвестная команда {commandName}");
+                _userInterface.WriteLine("Для справки введите help, для выхода - quit");
+                continue;
+            }
+            try
+            {
+                command.Execute(args);
+            }
+            catch (Exception err)
+            {
+                _userInterface.WriteLine($"При выполнении команды {commandName} произошла ошибка");
+                _userInterface.WriteLine(err.Message);
             }
         }
         while (can_work);
